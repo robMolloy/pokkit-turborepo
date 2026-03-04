592b74c138b69785e691b96a268c62aa6ee69f9d
robmolloy <robmolloy@hotmail.co.uk>
2026-03-04 19:18:42 +0000

    add shadcn to pokkit-components and use in pokkit-whisper

--- apps/pokkit-whisper/src/main.tsx
+++ apps/pokkit-whisper/src/main.tsx
@@ -1,5 +1,5 @@
 import { useReactiveAuthStore, useReactiveAuthStoreSync } from "@repo/pokkit-auth";
-import { Button } from "@repo/pokkit-components";
+import { Button, Input } from "@repo/pokkit-components";
 import "@repo/pokkit-components/styles.css";
 import PocketBase from "pocketbase";
 import { createRoot } from "react-dom/client";
@@ -11,53 +11,82 @@ const App = () => {
   const authStore = useReactiveAuthStore();
 
   return (
-    <div>
-      <Button>new</Button>
-      <button
-        onClick={async () => {
-          const resp = await pb.health.check();
-          console.log(resp);
-        }}
-      >
-        check health
-      </button>
-      <button
-        onClick={() => {
-          pb.collection("users").create({
-            email: "admin@admin.com",
-            password: "admin@admin.com",
-            passwordConfirm: "admin@admin.com",
-          });
-        }}
-      >
-        create user
-      </button>
-      <button
-        onClick={() => {
-          pb.collection("users").authWithPassword("admin@admin.com", "admin@admin.com");
-        }}
-      >
-        log in
-      </button>
-      <button
-        onClick={() => {
-          pb.authStore.clear();
-        }}
-      >
-        log out
-      </button>
-      <button
-        onClick={() => {
-          if (!authStore?.record.id) return;
+    <div className="bg-primary">
+      <div className="flex gap-4">
+        <button>html</button>
+        <Button>blank</Button>
+        <Button variant="default">default</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost" className="text-primary-foreground">
+          ghost
+        </Button>
+        <Button variant="link" className="text-primary-foreground">
+          link
+        </Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+      </div>
+      <div className="flex gap-4 bg-secondary">
+        <button>html</button>
+        <Button>blank</Button>
+        <Button variant="default">default</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="link">link</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+      </div>
+      <Input placeholder="placeholder" />
+      <input placeholder="placeholder" />
 
-          pb.collection("users").update(authStore.record.id, {
-            name: `updated name ${Math.floor(Math.random() * 1000)}`,
-          });
-        }}
-      >
-        update user
-      </button>
-      <pre>{JSON.stringify(authStore, undefined, 2)}</pre>
+      <br />
+      <div>
+        <button
+          onClick={async () => {
+            const resp = await pb.health.check();
+            console.log(resp);
+          }}
+        >
+          check health
+        </button>
+        <button
+          onClick={() => {
+            pb.collection("users").create({
+              email: "admin@admin.com",
+              password: "admin@admin.com",
+              passwordConfirm: "admin@admin.com",
+            });
+          }}
+        >
+          create user
+        </button>
+        <button
+          onClick={() => {
+            pb.collection("users").authWithPassword("admin@admin.com", "admin@admin.com");
+          }}
+        >
+          log in
+        </button>
+        <button
+          onClick={() => {
+            pb.authStore.clear();
+          }}
+        >
+          log out
+        </button>
+        <button
+          onClick={() => {
+            if (!authStore?.record.id) return;
+
+            pb.collection("users").update(authStore.record.id, {
+              name: `updated name ${Math.floor(Math.random() * 1000)}`,
+            });
+          }}
+        >
+          update user
+        </button>
+      </div>
+      <pre className="text-primary-foreground">{JSON.stringify(authStore, undefined, 2)}</pre>
     </div>
   );
 };
--- packages/pokkit-components/src/components/index.ts
+++ packages/pokkit-components/src/components/index.ts
@@ -1,1 +1,2 @@
-export * from "./button";
+export * from "./ui/input";
+export * from "./ui/button";
