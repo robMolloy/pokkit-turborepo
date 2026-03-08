cb766af221ed4cc11bfa5fddcda81f600c88cb2f
robmolloy <robmolloy@hotmail.co.uk>
2026-03-08 23:22:28 +0000

    Improved provider icon map

--- packages/pokkit-auth/src/components/SignUpOrSignInOAuth2Options.tsx
+++ packages/pokkit-auth/src/components/SignUpOrSignInOAuth2Options.tsx
@@ -1,13 +1,70 @@
 import { Button } from "@repo/pokkit-shadcn";
 import PocketBase, { AuthMethodsList } from "pocketbase";
-import { FaApple, FaGithub, FaGoogle } from "react-icons/fa6";
+import {
+  SiApple,
+  SiGoogle,
+  SiGithub,
+  SiFacebook,
+  SiInstagram,
+  SiGitlab,
+  SiBitbucket,
+  SiGitee,
+  SiGitea,
+  SiDiscord,
+  SiX,
+  SiKakaotalk,
+  SiVk,
+  SiLinear,
+  SiNotion,
+  SiBox,
+  SiSpotify,
+  SiTrakt,
+  SiTwitch,
+  SiPatreon,
+  SiStrava,
+  SiWakatime,
+  SiLivechat,
+  SiMaildotru,
+  SiOpenid,
+} from "react-icons/si";
+import { FaMicrosoft, FaYandex, FaCalendar, FaComment } from "react-icons/fa6";
 import { signUpOrSignInWithOAuth2 } from "../utils";
 import { IconType } from "react-icons";
 
 const providerIconMap: { [key: string]: IconType } = {
-  apple: FaApple,
-  google: FaGoogle,
-  github: FaGithub,
+  apple: SiApple,
+  google: SiGoogle,
+  github: SiGithub,
+  facebook: SiFacebook,
+  instagram: SiInstagram,
+  gitlab: SiGitlab,
+  bitbucket: SiBitbucket,
+  gitee: SiGitee,
+  gitea: SiGitea,
+  discord: SiDiscord,
+  twitter: SiX,
+  x: SiX,
+  kakao: SiKakaotalk,
+  vk: SiVk,
+  linear: SiLinear,
+  notion: SiNotion,
+  microsoft: FaMicrosoft, // not available as simple icons
+  yandex: FaYandex, // not available as simple icons
+  monday: FaCalendar, // not available as simple icons
+  lark: FaComment, // not available as simple icons
+  box: SiBox,
+  spotify: SiSpotify,
+  trakt: SiTrakt,
+  twitch: SiTwitch,
+  patreon: SiPatreon,
+  strava: SiStrava,
+  wakatime: SiWakatime,
+  livechat: SiLivechat,
+  mailcow: SiMaildotru,
+  planningcenter: SiOpenid, // Using OpenID as fallback
+  oidc: SiOpenid,
+  oidc2: SiOpenid,
+  oidc3: SiOpenid,
 };
 
 export const SignUpOrSignInOAuth2Options = (p: {
