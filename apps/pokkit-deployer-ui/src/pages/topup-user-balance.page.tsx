dec1b07548c266cf3ecd48afddb6669cbd9c0a56
robmolloy <robmolloy@hotmail.co.uk>
2026-05-01 11:55:50 +0100

    improve pokkit-component button

--- apps/pokkit-deployer-ui/src/pages/topup-user-balance.page.tsx
+++ apps/pokkit-deployer-ui/src/pages/topup-user-balance.page.tsx
@@ -1,97 +1,76 @@
 import { pb } from "@/config/pocketbaseConfig";
 import { createToastProps } from "@/lib/createToastProps";
 import { createStripeCheckoutSession } from "@/modules/stripe/stripeSdk";
-import { Button } from "@repo/pokkit-shadcn";
+import { Button } from "@repo/pokkit-components";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div>
-      <Button
-        type="button"
-        onClick={async () => {
-          const resp = (await pb.send("/hello/rob", { method: "GET" })) as Response;
-          console.log(`topup-user-balance.page.tsx:${/*LL*/ 39}`, resp);
-        }}
-      >
-        hit hello endpoint
-      </Button>
-      <Button
-        type="button"
-        onClick={async () => {
-          const resp = await pb.send("/bye", {
-            method: "POST",
-            body: JSON.stringify({ name: "robby" }),
-          });
-          console.log(`topup-user-balance.page.tsx:${/*LL*/ 51}`, resp);
-        }}
-      >
-        hit bye endpoint
-      </Button>
-      <Button
-        onClick={async () => {
-          const sessionResp = await createStripeCheckoutSession({
-            productName: "token",
-            quantity: 100,
-          });
+    <span>
+      <span className="inline-flex flex-col gap-8">
+        <Button
+          loading={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            const resp = (await pb.send("/hello/rob", { method: "GET" })) as Response;
+            console.log(`topup-user-balance.page.tsx:${/*LL*/ 39}`, resp);
+            setIsLoading(false);
+          }}
+        >
+          hit hello endpoint
+        </Button>
+        <Button
+          loading={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            const resp = await pb.send("/bye", {
+              method: "POST",
+              body: JSON.stringify({ name: "robby" }),
+            });
+            console.log(`topup-user-balance.page.tsx:${/*LL*/ 51}`, resp);
+            setIsLoading(false);
+          }}
+        >
+          hit bye endpoint
+        </Button>
+        <Button
+          loading={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            const sessionResp = await createStripeCheckoutSession({
+              productName: "token",
+              quantity: 100,
+            });
 
-          if (!sessionResp.success) return toast.error(...createToastProps(sessionResp.messages));
+            if (!sessionResp.success) return toast.error(...createToastProps(sessionResp.messages));
 
-          window.location.href = sessionResp.data.url;
-        }}
-      >
-        Top up 100
-      </Button>
-      <Button
-        onClick={async () => {
-          const sessionResp = await createStripeCheckoutSession({
-            productName: "instance_subscription",
-            quantity: 100,
-          });
+            window.location.href = sessionResp.data.url;
+            setIsLoading(false);
+          }}
+        >
+          Top up 100
+        </Button>
+        <Button
+          loading={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            const sessionResp = await createStripeCheckoutSession({
+              productName: "instance_subscription",
+              quantity: 100,
+            });
 
-          if (!sessionResp.success) return toast.error(...createToastProps(sessionResp.messages));
+            if (!sessionResp.success) return toast.error(...createToastProps(sessionResp.messages));
 
-          window.location.href = sessionResp.data.url;
-        }}
-      >
-        create a subscription
-      </Button>
-    </div>
+            window.location.href = sessionResp.data.url;
+            setIsLoading(false);
+          }}
+        >
+          create a subscription
+        </Button>
+      </span>
+    </span>
   );
 }
-
-// const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY as string);
-
-// const TOKEN_AMOUNT = 100;
-
-// export function BuyTokensButton() {
-//   const handleBuy = async () => {
-//     const stripe = await stripePromise;
-//     if (!stripe) return console.error("Stripe failed to load.");
-
-//     const res = await fetch("/api/create-payment-intent", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ tokens: TOKEN_AMOUNT }),
-//     });
-
-//     const data = await res.json();
-//     console.log("PaymentIntent response:", data);
-
-//     const { error } = await stripe.confirmPayment({
-//       clientSecret: data.clientSecret,
-//       confirmParams: {
-//         return_url: `${window.location.origin}/payment-success`,
-//       },
-//     });
-
-//     console.log("Stripe result:", error ?? "redirecting…");
-//   };
-
-//   return (
-//     <button onClick={handleBuy}>
-//       <span>◈</span>
-//       Buy {TOKEN_AMOUNT} Tokens
-//     </button>
-//   );
-// }
--- packages/pokkit-components/src/components/button.tsx
+++ packages/pokkit-components/src/components/button.tsx
@@ -1,8 +1,37 @@
 import { Button as CnButton, cn } from "@repo/pokkit-shadcn";
+import { CustomIcon } from "./CustomIcon";
 
 type TCnButtonProps = React.ComponentProps<typeof CnButton>;
 
-export const Button = (p: TCnButtonProps) => {
-  const { className, ...rest } = p;
-  return <CnButton className={cn("cursor-pointer", className)} {...rest} />;
+export const Button = (
+  p: TCnButtonProps & { loading?: boolean; loadingContent?: React.ReactNode },
+) => {
+  const { className, loading, children, disabled, ...rest } = p;
+
+  return (
+    <CnButton
+      className={cn("cursor-pointer", className)}
+      disabled={disabled ? disabled : loading}
+      {...rest}
+    >
+      {loading === undefined ? (
+        p.children
+      ) : (
+        <div className="grid">
+          <span
+            className={cn(
+              "col-start-1 row-start-1 flex justify-center items-center gap-2",
+              !p.loading && "invisible",
+            )}
+          >
+            <CustomIcon iconName="Loader" className="animate-spin" size="sm" />
+            <span>{p.loadingContent ? p.loadingContent : p.children}</span>
+          </span>
+          <span className={cn("col-start-1 row-start-1", p.loading && "invisible")}>
+            {p.children}
+          </span>
+        </div>
+      )}
+    </CnButton>
+  );
 };
