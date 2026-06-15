87c29a668b4890613a352120c3308944fab8dbff
robmolloy <robmolloy@hotmail.co.uk>
2026-06-15 21:01:30 +0100

    create TStripeProductPriceRecordProxy

--- apps/pokkit-deployer-db/src/modules/stripeProductPricesSdk/stripeProductPricesSdk.go
+++ apps/pokkit-deployer-db/src/modules/stripeProductPricesSdk/stripeProductPricesSdk.go
@@ -10,6 +10,67 @@ import (
 	pbTypes "github.com/pocketbase/pocketbase/tools/types"
 )
 
+var _ pbCore.RecordProxy = (*TStripeProductPriceRecordProxy)(nil)
+
+type TStripeProductPriceRecordProxy struct {
+	pbCore.BaseRecordProxy
+}
+
+func (a *TStripeProductPriceRecordProxy) GetId() string {
+	return a.GetString("id")
+}
+func (a *TStripeProductPriceRecordProxy) SetId(value string) {
+	a.Set("id", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetStripeProductId() string {
+	return a.GetString("stripeProductId")
+}
+func (a *TStripeProductPriceRecordProxy) SetStripeProductId(value string) {
+	a.Set("stripeProductId", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetStripePriceId() string {
+	return a.GetString("stripePriceId")
+}
+func (a *TStripeProductPriceRecordProxy) SetStripePriceId(value string) {
+	a.Set("stripePriceId", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetRecurrenceInterval() string {
+	return a.GetString("recurrenceInterval")
+}
+func (a *TStripeProductPriceRecordProxy) SetRecurrenceInterval(value string) {
+	a.Set("recurrenceInterval", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetRecurrenceIntervalCount() int {
+	return a.GetInt("recurrenceIntervalCount")
+}
+func (a *TStripeProductPriceRecordProxy) SetRecurrenceIntervalCount(value int) {
+	a.Set("recurrenceIntervalCount", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetCurrency() string {
+	return a.GetString("currency")
+}
+func (a *TStripeProductPriceRecordProxy) SetCurrency(value string) {
+	a.Set("currency", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetCostPerUnit() int {
+	return a.GetInt("costPerUnit")
+}
+func (a *TStripeProductPriceRecordProxy) SetCostPerUnit(value int) {
+	a.Set("costPerUnit", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetCreated() pbTypes.DateTime {
+	return a.GetDateTime("created")
+}
+func (a *TStripeProductPriceRecordProxy) SetCreated(value pbTypes.DateTime) {
+	a.Set("created", value)
+}
+func (a *TStripeProductPriceRecordProxy) GetUpdated() pbTypes.DateTime {
+	return a.GetDateTime("updated")
+}
+func (a *TStripeProductPriceRecordProxy) SetUpdated(value pbTypes.DateTime) {
+	a.Set("updated", value)
+}
+
 type TStripeProductPriceRecordStruct struct {
 	Id                      string           `json:"id"`
 	StripeProductId         string           `json:"stripeProductId"`
