54de8ac7f9efcdd0bc23199a7d0bd759d407d9ca
robmolloy <robmolloy@hotmail.co.uk>
2026-08-01 23:14:58 +0100

    add pokkit-starter-docs

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export const Button = (p: { appName?: string; className?: string; children?: React.ReactNode }) => {
-  return <button {...p}>button</button>;
+export const Button = (p: { className?: string; children?: React.ReactNode }) => {
+  const { className, children } = p;
+  return <button className={className}>{children}</button>;
 };
