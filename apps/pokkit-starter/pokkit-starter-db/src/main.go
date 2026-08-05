656dfa4a0d33a5a83e7ca5d60b56f92daf51cbe3
robmolloy <robmolloy@hotmail.co.uk>
2026-08-05 07:10:40 +0100

    feat-and-tests/access-logs

--- apps/pokkit-starter/pokkit-starter-db/src/main.go
+++ apps/pokkit-starter/pokkit-starter-db/src/main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"log"
 
-	pokkitDbConfigSync "github.com/robMolloy/pokkit-turborepo/packages/pokkitDbConfigSync"
-	pokkitDbPermissions "github.com/robMolloy/pokkit-turborepo/packages/pokkitDbPermissions"
-
 	pocketbase "github.com/pocketbase/pocketbase"
 	pbCore "github.com/pocketbase/pocketbase/core"
+	"github.com/robMolloy/pokkit-turborepo/packages/pokkitDbConfigSync"
 )
 
 func main() {
 	app := pocketbase.New()
 
 	pokkitDbConfigSync.BindFunctions(app)
-	pokkitDbPermissions.BindFunctions(app)
+	// pokkitDbPermissions.BindFunctions(app)
 
 	app.OnServe().BindFunc(func(se *pbCore.ServeEvent) error {
 		return se.Next()
