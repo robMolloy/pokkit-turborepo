c790c51b59d71ecfc3908ecb10f8336034409717
robmolloy <robmolloy@hotmail.co.uk>
2026-08-06 23:13:16 +0100

    pokkit-db-config-sync-settings-file-valid tests with settings changes

--- apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/pokkit-db-config-sync-tests/pokkit-db-config-sync-settings-file-missing.test.ts
+++ apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/pokkit-db-config-sync-tests/pokkit-db-config-sync-settings-file-missing.test.ts
@@ -16,7 +16,7 @@ import { pokkitDbConfigSyncTestsMetadata } from "./_pokkitDbConfigSyncTestsMetad
 import { validSettingsFileData } from "./mocks/validSettingsFileData";
 import { safeJsonParse } from "@repo/pokkit-utils";
 
-const testMetadata = pokkitDbConfigSyncTestsMetadata.settingsFileValid;
+const testMetadata = pokkitDbConfigSyncTestsMetadata.settingsFileMissing;
 const testSuiteName = testMetadata.name;
 
 const pbPortNumber = testMetadata.portNumber;
@@ -27,7 +27,7 @@ const logFilePath = `_logs/${testSuiteName}`;
 
 const createPbConnection = () => new PocketBase(pbServeUrl);
 
-let settingsFileContentsAtStart = "";
+// let settingsFileContentsAtStart = "";
 
 describe(`${testSuiteName} tests`, () => {
   beforeAll(async () => {
@@ -35,15 +35,11 @@ describe(`${testSuiteName} tests`, () => {
     fse.removeSync(pbDirPath);
     fse.copySync(sourceTestBuildDirPath, pbDirPath);
     fse.removeSync(getPokkitDbSettingsFilePath({ pbDirPath }));
-    fse.writeFileSync(
-      getPokkitDbSettingsFilePath({ pbDirPath }),
-      JSON.stringify(validSettingsFileData, null, 2),
-    );
 
-    settingsFileContentsAtStart = fse.readFileSync(
-      getPokkitDbSettingsFilePath({ pbDirPath }),
-      "utf8",
-    );
+    // settingsFileContentsAtStart = fse.readFileSync(
+    //   getPokkitDbSettingsFilePath({ pbDirPath }),
+    //   "utf8",
+    // );
 
     await servePb({ pbFilePath, pbPortNumber, logFilePath });
     await upsertPbAdminCredentialsFromCli({ pbFilePath, superuserEmail, superuserPassword });
@@ -64,43 +60,12 @@ describe(`${testSuiteName} tests`, () => {
     expect(isHealthy.code).toBe(200);
   });
 
-  it("PDBCS-SET-01 — Valid settings file imports on startup", async () => {
+  it("PDBCS-SET-03 — Missing settings file leaves settings unchanged", async () => {
     const superuserPb = createPbConnection();
     await superuserPb
       .collection(superusersCollectionName)
       .authWithPassword(superuserEmail, superuserPassword);
     const settings = await superuserPb.settings.getAll();
-    expect(settings).toEqual(validSettingsFileData);
-  });
-
-  it("PDBCS-SET-05 — Valid settings file unchanged when already in sync", async () => {
-    const superuserPb = createPbConnection();
-    await superuserPb
-      .collection(superusersCollectionName)
-      .authWithPassword(superuserEmail, superuserPassword);
-
-    const settingsFileContents = fse.readFileSync(
-      getPokkitDbSettingsFilePath({ pbDirPath }),
-      "utf8",
-    );
-    expect(settingsFileContents).toEqual(settingsFileContentsAtStart);
-  });
-
-  it("PDBCS-SET-04 — When settings are changed at runtime, the settings file is updated", async () => {
-    const newAppName = "NEW_APP_NAME";
-    const superuserPb = createPbConnection();
-    await superuserPb
-      .collection(superusersCollectionName)
-      .authWithPassword(superuserEmail, superuserPassword);
-
-    const settingsAtStart = await superuserPb.settings.getAll();
-    expect(settingsAtStart).toEqual(validSettingsFileData);
-
-    await superuserPb.settings.update({
-      meta: { ...settingsAtStart.meta, appName: newAppName },
-    });
-    const settingsAtEnd = await superuserPb.settings.getAll();
-    expect(settingsAtEnd.meta.appName).toEqual(newAppName);
 
     const settingsFileContents = fse.readFileSync(
       getPokkitDbSettingsFilePath({ pbDirPath }),
@@ -108,8 +73,6 @@ describe(`${testSuiteName} tests`, () => {
     );
     const settingsFileContentsJson = safeJsonParse(settingsFileContents);
     expect(settingsFileContentsJson.success).toEqual(true);
-    expect((settingsFileContentsJson.data as { meta: { appName: string } }).meta.appName).toEqual(
-      newAppName,
-    );
+    expect(settingsFileContentsJson.data).toEqual(settings);
   });
 });
