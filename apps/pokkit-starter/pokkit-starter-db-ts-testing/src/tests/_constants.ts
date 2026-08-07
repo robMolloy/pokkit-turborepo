eace20345d2c0499278685fa49e3d326f711515d
robmolloy <robmolloy@hotmail.co.uk>
2026-08-07 11:09:26 +0100

    use platform-specific paths to untouched PB in tests

--- apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/_constants.ts
+++ apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/_constants.ts
@@ -1,9 +1,14 @@
+import os from "node:os";
+
 export const sourceTestBuildDirPath = "./source-test-build";
 export const getUntouchedSourcePbForPlatformDirPath = () => {
   const platform = process.platform;
+  const arch = os.arch();
 
-  if (platform === "darwin") return "./source-untouched-pb-build/mac";
-  if (platform === "linux") return "./source-untouched-pb-build/linux";
+  if (platform === "darwin" && arch === "arm64") return "./source-untouched-pb-build/darwin-arm64";
+  if (platform === "darwin" && arch === "x64") return "./source-untouched-pb-build/darwin-x64";
+  if (platform === "linux" && arch === "arm64") return "./source-untouched-pb-build/linux-arm64";
+  if (platform === "linux" && arch === "x64") return "./source-untouched-pb-build/linux-x64";
 
   throw new Error(`Unsupported platform: ${platform}`);
 };
