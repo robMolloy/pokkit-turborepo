bbd89e18d8184b9176966bd6d05ecd59b63024b3
robmolloy <robmolloy@hotmail.co.uk>
2026-08-11 21:48:39 +0100

    Add user collection permission tests for CREATE, VIEW, LIST, UPDATE, and DELETE actions

--- apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/pokkit-db-permissions/pokkit-db-permissions-users-collection-create-action.test.ts
+++ apps/pokkit-starter/pokkit-starter-db-ts-testing/src/tests/pokkit-db-permissions/pokkit-db-permissions-users-collection-create-action.test.ts
@@ -48,9 +48,109 @@ describe(`${testSuiteName} tests`, () => {
     });
   });
 
-  it("is connection healthy", async () => {
+  it.todo("is connection healthy", async () => {
     const pb = createPbConnection();
     const isHealthy = await pb.health.check();
     expect(isHealthy.code).toBe(200);
   });
+
+  it("PDBP-USERS-SETUP-01 — Verify collection presence and validity is setup correctly", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-01 — Global Superadmin can CREATE", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-02 — Global Admin cannot CREATE", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-03 — Global Standard cannot CREATE", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-OWN-01 — Global Superadmin cannot CREATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-OWN-02 — Global Admin cannot CREATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-CREATE-OWN-03 — Global Standard cannot CREATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-01 — Global Superadmin can VIEW", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-02 — Global Admin (approved) can VIEW", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-03 — Global Admin (pending or blocked) cannot VIEW", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-04 — Global Standard (approved) can VIEW", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-05 — Global Standard (pending or blocked) cannot VIEW", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-OWN-01 — Global Superadmin can VIEW OWN", async () => {});
+
+  it.todo("PDBP-USERS-VIEW-OWN-02 — Global Admin (approved) can VIEW OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-VIEW-OWN-03 — Global Admin (pending or blocked) cannot VIEW OWN",
+    async () => {},
+  );
+
+  it.todo("PDBP-USERS-VIEW-OWN-04 — Global Standard (approved) can VIEW OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-VIEW-OWN-05 — Global Standard (pending or blocked) cannot VIEW OWN",
+    async () => {},
+  );
+
+  it.todo("PDBP-USERS-LIST-01 — Global Superadmin can LIST", async () => {});
+
+  it.todo("PDBP-USERS-LIST-02 — Global Admin (approved) can LIST", async () => {});
+
+  it.todo("PDBP-USERS-LIST-03 — Global Admin (pending or blocked) cannot LIST", async () => {});
+
+  it.todo("PDBP-USERS-LIST-04 — Global Standard (approved) can LIST", async () => {});
+
+  it.todo("PDBP-USERS-LIST-05 — Global Standard (pending or blocked) cannot LIST", async () => {});
+
+  it.todo("PDBP-USERS-LIST-OWN-01 — Global Superadmin can LIST OWN", async () => {});
+
+  it.todo("PDBP-USERS-LIST-OWN-02 — Global Admin (approved) can LIST OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-LIST-OWN-03 — Global Admin (pending or blocked) cannot LIST OWN",
+    async () => {},
+  );
+
+  it.todo("PDBP-USERS-LIST-OWN-04 — Global Standard (approved) can LIST OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-LIST-OWN-05 — Global Standard (pending or blocked) cannot LIST OWN",
+    async () => {},
+  );
+
+  it.todo("PDBP-USERS-UPDATE-01 — Global Superadmin can UPDATE", async () => {});
+
+  it.todo("PDBP-USERS-UPDATE-02 — Global Admin cannot UPDATE", async () => {});
+
+  it.todo("PDBP-USERS-UPDATE-03 — Global Standard cannot UPDATE", async () => {});
+
+  it.todo("PDBP-USERS-UPDATE-OWN-01 — Global Superadmin cannot UPDATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-UPDATE-OWN-02 — Global Admin cannot UPDATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-UPDATE-OWN-03 — Global Standard cannot UPDATE OWN", async () => {});
+
+  it.todo("PDBP-USERS-DELETE-01 — Global Superadmin can DELETE", async () => {});
+
+  it.todo("PDBP-USERS-DELETE-02 — Global Admin cannot DELETE", async () => {});
+
+  it.todo("PDBP-USERS-DELETE-03 — Global Standard cannot DELETE", async () => {});
+
+  it.todo("PDBP-USERS-DELETE-OWN-01 — Global Superadmin cannot DELETE OWN", async () => {});
+
+  it.todo("PDBP-USERS-DELETE-OWN-02 — Global Admin (approved) can DELETE OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-DELETE-OWN-03 — Global Admin (pending or blocked) cannot DELETE OWN",
+    async () => {},
+  );
+
+  it.todo("PDBP-USERS-DELETE-OWN-04 — Global Standard (approved) can DELETE OWN", async () => {});
+
+  it.todo(
+    "PDBP-USERS-DELETE-OWN-05 — Global Standard (pending or blocked) cannot DELETE OWN",
+    async () => {},
+  );
 });
