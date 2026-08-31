338e3c993fb3515e0c93c5a6b4bbe6f487d54910
robmolloy <robmolloy@hotmail.co.uk>
2026-08-31 20:28:34 +0100

    pokkitstarter: pokkit-starter-deploy-build-on-merge github action

---
+++ apps/pokkit-starter/pokkit-starter-deploy/README.md
@@ -0,0 +1,3 @@
+# Pokkit Utils
+
+simple utils
---
+++ apps/pokkit-starter/pokkit-starter-deploy/src/deploy-build.js
@@ -0,0 +1,26 @@
+import PocketBase from "pocketbase";
+import fs from "fs-extra";
+
+const pb = new PocketBase("https://pokkit.cloud:9999/");
+
+await (async () => {
+  const collectionsBuffer = fs.readFileSync("build/pb_config/collections.json");
+  const secretsBuffer = fs.readFileSync("build/pb_config/secrets.json");
+  const settingsBuffer = fs.readFileSync("build/pb_config/settings.json");
+  const collectionsFile = collectionsBuffer
+    ? new File([collectionsBuffer], "collections.json")
+    : undefined;
+  const secretsFile = secretsBuffer ? new File([secretsBuffer], "secrets.json") : undefined;
+  const settingsFile = settingsBuffer ? new File([settingsBuffer], "settings.json") : undefined;
+
+  const deployment = await pb.collection("deployments").create({
+    buildFile: fs.readFileSync("build/app-db"),
+    collectionsFile,
+    secretsFile,
+    settingsFile,
+    superuserEmail: "admin@admin.com",
+    superuserPassword: "admin@admin.com",
+  });
+
+  console.log(deployment);
+})();
